04ca471625ff6bb44ad0d868d40fbb40aee773f3
Heliton Bruning <heliton3304@gmail.com>
2025-05-01 19:06:34 -0300

    Delete fire_incidents_dbt/macros/freshness_test.sql

--- fire_incidents_dbt/macros/freshness_test.sql
+++
@@ -1,6 +0,0 @@
-{% macro test_freshness(model, threshold_hours=24) %}
-    SELECT *
-    FROM {{ model }}
-    WHERE last_updated_at < CURRENT_TIMESTAMP - INTERVAL '{{ threshold_hours }} hours'
-    LIMIT 1
-{% endmacro %}
